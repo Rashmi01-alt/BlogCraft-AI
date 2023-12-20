a2f2fb8e6844b2c3c033ff75acb914ed54a3c4b0
starringRashmi <purkayastharashmi@gmail.com>
2023-12-20 17:17:44 +0530

    header added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import BlogForm from "./components/BlogForm";
+import BlogList from "./components/BlogList";
+
+const App = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+      <div className="container mx-auto py-8">
+        <BlogForm />
+        <BlogList />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/BlogForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const BlogForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Call your backend API to handle the creation of blogs
+    await axios.post("/api/blogs", { title, content });
+    // Optionally, you can reset the form fields after submission
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <div className="mb-8">
+      <h2 className="text-2xl font-semibold mb-4">Create a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
+            Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="content"
+          >
+            Content
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
