172d1be915abb7160ef714383b8c66d3c0d45899
starringRashmi <purkayastharashmi@gmail.com>
2023-12-24 19:05:20 +0530

    routes updated

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-
 const { getAllBlogs, createBlog } = require("../controllers/blogcontroller");
 
-router.get("/get" ,getAllBlogs);
+router.get("/get", getAllBlogs);
 router.post("/create", createBlog);
 
 module.exports = router;
