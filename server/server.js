7ac44e6e5c6cbd77f171456687aaa80a3b44620c
starringRashmi <purkayastharashmi@gmail.com>
2024-01-04 23:43:43 +0530

    project completed

--- server/server.js
+++ server/server.js
@@ -4,15 +4,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const { dbconnect } = require("./config/database");
 
-const blogRoutes  = require("./routes/blogRoutes");
-
+const blogRoutes = require("./routes/blogRoutes");
 
 const PORT = 5000;
 dbconnect();
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
-
 
 app.use("/api/blogs", blogRoutes);
 
